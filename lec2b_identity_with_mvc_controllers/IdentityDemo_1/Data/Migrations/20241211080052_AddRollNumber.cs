246fe0c7eaf5b7f5cc43766c9428562cad9883b9
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2024-12-11 20:25:13 +0500

    identity with mvc controller

---
+++ lec2b_identity_with_mvc_controllers/IdentityDemo_1/Data/Migrations/20241211080052_AddRollNumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityDemo_1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRollNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RollNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RollNumber",
+                table: "AspNetUsers");
+        }
+    }
+}
